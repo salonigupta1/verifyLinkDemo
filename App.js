a32a69ec33441bcf00aed3173d1a753b1b12ea62
Saloni Gupta <saloniguptasg12@gmail.com>
2021-07-12 17:40:24 +0530

    Initial repo

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
+import { NavigationContainer } from '@react-navigation/native';
+
+const prefix = Linking.createURL('/');
 
 export default function App() {
+  const linking = {
+    prefixes: ['https://*.example.com', 'https://example.com'],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      {/* content */}
+    </NavigationContainer>
   );
 }
 
